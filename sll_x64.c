9c6aad55937938ca46bb157e86ab2c87278f54a2
rodgerswisdom <trulyhawona@gmail.com>
2025-02-06 22:44:51 +0300

    Automated commit

--- sll_x64.c
+++ sll_x64.c
@@ -1,31 +1,49 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+#define DATA_1 5
+#define DATA_2 6
+
 /**
  * Singly Linked list
  */
-typedef struct Node{
+typedef struct Node
+{
     int data;
     struct Node *next;
-};
+} Node;
+
+
+void printlist(Node *p);
+
+void printlist(Node *p)
+{
+    printf("\n\nList elements are - \n");
+    while(p != NULL) {
+        printf("%d --->",p->data);
+        p = p->next;
+    }
+}
 
 void main()
 {
-    struct Node *head = NULL;
-    struct Node *one = NULL;
-    struct Node *two = NULL;
-    struct Node *null = NULL;
+    Node *head = NULL;
+    Node *one = NULL;
+    Node *two = NULL;
+    Node *null = NULL;
 
 
-    one = (struct Node*)malloc(sizeof(struct Node*));
-    two = (struct Node*)malloc(sizeof(struct Node*));
+    one = (Node*)malloc(sizeof(Node*));
+    two = (Node*)malloc(sizeof(Node*));
 
-    one->data = 1;
-    two->data = 2;
+    one->data = DATA_1;
+    two->data = DATA_2;
 
     one->next = two;
     two->next = null;
 
     head = one;
 
-
+    printlist(head);
 
 }
